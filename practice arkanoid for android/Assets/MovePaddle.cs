bd00c114d9be0dd5532f779cc64b9c228ec8a582
Fangszx <jygrzn@gmail.com>
2021-02-05 21:19:26 +0800

    added on touch controls
    paddle will follow the x value of the touch positions x.
    used Time.deltaTime to smoothen the movement

---
+++ practice arkanoid for android/Assets/MovePaddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+
+    //config params
+    [SerializeField] float speed;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            //get touch position x
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            Debug.Log(touchPosition.x);
+            Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+            
+
+            if(paddlePos.x < touchPosition.x)
+            {
+                paddlePos.x++;
+            }
+            else
+            {
+                paddlePos.x--;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, paddlePos, Time.deltaTime * speed);
+            
+        }
+    }
+}
